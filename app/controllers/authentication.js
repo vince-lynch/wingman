f6ea331981cf515c251b4fcbb487133fec501279
root <root@vps307308.ovh.net>
2016-12-25 21:00:12 +0000

    jsonwebtokens stopped working, I was using an outdated syntax

--- app/controllers/authentication.js
+++ app/controllers/authentication.js
@@ -24,7 +24,7 @@ function register(req, res) {
       return res.status(400).json({ message: err.toString() });
     }
 
-    var token = jwt.sign(user, secret, "24h");
+   var token = jwt.sign(user, secret, { expiresIn: 60 * 60 });
     return res.status(200).json({ message: "Thanks for registering", user: user, token: token });
   });
 }
@@ -37,8 +37,13 @@ function login(req, res) {
     if(err) return res.send(500).json({ message: err });
     if(!user || !user.validatePassword(req.body.password)) return res.status(401).json({ message: "Unauthorized" });
 
-    var token = jwt.sign(user, secret, "24h");
-    return res.status(200).json({ message: "Login successful", user: user, token: token });
+    console.log("the secret: ", secret)
+
+    var token = jwt.sign(user, secret, { expiresIn: 60 * 60 });
+
+    console.log("token is: ", token)
+   // var token = jwt.sign(user, secret, "24h");
+    return res.status(200).json({ message: "Login successful", user: user, token: token }); //
   });
 }
 
